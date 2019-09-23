3d632474773971fddc1e476f805cd48883d9ce60
toppev <44984598+toppev@users.noreply.github.com>
2019-09-23 20:57:13 +0300

    Use lombok

--- src/main/java/ga/strikepractice/leaderboards/SimpleIcon.java
+++ src/main/java/ga/strikepractice/leaderboards/SimpleIcon.java
@@ -1,40 +1,15 @@
 package ga.strikepractice.leaderboards;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
 import org.bukkit.inventory.ItemStack;
 
+@Data
+@AllArgsConstructor
 public class SimpleIcon {
 
     private ItemStack item;
     private String tag;
     private int slot;
 
-    public SimpleIcon(ItemStack item, String tag, int slot) {
-        this.tag = tag.toLowerCase().replace("-", "_");
-        this.slot = slot;
-        this.item = item;
-    }
-
-    public ItemStack getItem() {
-        return item;
-    }
-
-    public String getTag() {
-        return tag;
-    }
-
-    public int getSlot() {
-        return slot;
-    }
-
-    public void setItem(ItemStack item) {
-        this.item = item;
-    }
-
-    public void setTag(String tag) {
-        this.tag = tag;
-    }
-
-    public void setSlot(int slot) {
-        this.slot = slot;
-    }
 }
--- src/main/java/ga/strikepractice/leaderboards/update/UpdateNotifier.java
+++ src/main/java/ga/strikepractice/leaderboards/update/UpdateNotifier.java
@@ -1,12 +1,6 @@
 package ga.strikepractice.leaderboards.update;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.URL;
-import java.net.URLConnection;
-import java.nio.charset.StandardCharsets;
-
+import lombok.Getter;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.event.EventHandler;
@@ -15,13 +9,21 @@ import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitRunnable;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+import java.nio.charset.StandardCharsets;
+
 public class UpdateNotifier implements Listener {
 
-    private boolean updateAvailable;
-
+    @Getter
+    private final String updatesURL;
     private final JavaPlugin plugin;
     private final String apiURL;
-    private final String updatesURL;
+    @Getter
+    private boolean updateAvailable;
 
     public UpdateNotifier(JavaPlugin plugin, int resourceId, boolean notifyStaff) {
         this.plugin = plugin;
@@ -36,8 +38,7 @@ public class UpdateNotifier implements Listener {
     @EventHandler
     public void onJoin(PlayerJoinEvent e) {
         if (e.getPlayer().isOp() && updateAvailable) {
-            e.getPlayer().sendMessage(ChatColor.GRAY + "There is a new update available for " + ChatColor.AQUA
-                    + plugin.getDescription().getName() + ChatColor.GRAY + ".");
+            e.getPlayer().sendMessage(ChatColor.GRAY + "There is a new update available for " + ChatColor.AQUA + plugin.getDescription().getName() + ChatColor.GRAY + ".");
             e.getPlayer().sendMessage(ChatColor.GRAY + "Link: " + updatesURL);
         }
     }
@@ -55,13 +56,11 @@ public class UpdateNotifier implements Listener {
     public boolean checkUpdates() {
         try {
             URLConnection conn = new URL(apiURL).openConnection();
-            conn.setRequestProperty("User-Agent",
-                    "Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.102 Safari/537.36");
+            conn.setRequestProperty("User-Agent", "Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.102 Safari/537.36");
             conn.setConnectTimeout(10000);
             conn.setReadTimeout(10000);
             conn.connect();
-            BufferedReader reader = new BufferedReader(
-                    new InputStreamReader(conn.getInputStream(), StandardCharsets.UTF_8));
+            BufferedReader reader = new BufferedReader(new InputStreamReader(conn.getInputStream(), StandardCharsets.UTF_8));
             StringBuilder builder = new StringBuilder();
             String str1;
             while ((str1 = reader.readLine()) != null) {
@@ -74,12 +73,12 @@ public class UpdateNotifier implements Listener {
                 Bukkit.getLogger().info("Link: " + updatesURL);
             }
         } catch (IOException e) {
-            Bukkit.getLogger().warning("Failed to check updates for " + plugin.getDescription().getName() + " "
-                    + plugin.getDescription().getVersion());
+            Bukkit.getLogger().warning("Failed to check updates for " + plugin.getDescription().getName() + " " + plugin.getDescription().getVersion());
         }
         return updateAvailable;
     }
 
+    @Deprecated
     public boolean wasUpdateAvailable() {
         return updateAvailable;
     }
